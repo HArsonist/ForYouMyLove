08ce1089e9a0492470af1226ad613aeea47b3493
HArsonist <nandrianinaharrischristophe@gmail.com>
2025-02-19 10:21:14 +0100

    Update script.js

--- V/script.js
+++ V/script.js
@@ -4,7 +4,7 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
-yesBtn.addEventListener("click", () => {
+Great! Can't wait to see you ❤️Btn.addEventListener("click", () => {
     question.innerHTML = "Being with you is my biggest blessing. I love you.";
     gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGNhdXh1b252b2F2b2U4cHRlNGkwMDZsajllaGF1cDJyb2p4NXl2YiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/G6N0pDDgDpLjUvNoyQ/giphy.gif";
 
